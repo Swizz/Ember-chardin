2df87647921e8150df14dfd26c8f58da0dfaec10
swizz <gerodel.quentin@gmail.com>
2016-03-04 11:50:46 +0100

    Fix weird style

--- addon/components/chardin-hint.js
+++ addon/components/chardin-hint.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName : 'span',
+  classNames: ['chardin-hint'],
 
   attributeBindings : ['hint:data-intro', 'position:data-position']
 });
