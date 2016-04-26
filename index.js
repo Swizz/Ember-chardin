01cfd97169e4f884e3422d995ff6bf7470f988ba
swizz <gerodel.quentin@gmail.com>
2016-04-26 10:37:29 +0200

    Import dependencies

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-chardin'
+  name: 'ember-chardin',
+
+  included: function(app, parentAddon) {
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+     this.app = app = app.app;
+    }
+    this._super.included(app);
+
+    var target = (parentAddon || app);
+    var options = target.options || {};
+
+    target.import(target.bowerDirectory + "/chardin.js/chardinjs.css");
+    target.import(target.bowerDirectory + "/chardin.js/chardinjs.js");
+  }
 };
